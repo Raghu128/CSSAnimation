64227c8dc3ccc81b62167cfb422ef63baeab3c53
Raghu128 <119107622+Raghu128@users.noreply.github.com>
2024-07-12 00:49:24 +0530

    Add the drap Animation

--- DragAndDrop/script.js
+++ DragAndDrop/script.js
@@ -1,25 +1,56 @@
-const box = document.querySelector(".box");
-let newX = 0, newY = 0, startX = 0, startY = 0;
+const boxes = document.querySelectorAll(".box");
+let newX = 0,
+  newY = 0,
+  startX = 0,
+  startY = 0;
+let currentBox = null;
+let initialTop = 0,
+  initialLeft = 0;
 
-box.addEventListener("mousedown", (e) => {
+boxes.forEach((box) => {
+  box.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    currentBox = box;
+
     startX = e.clientX;
     startY = e.clientY;
-    
-    document.addEventListener("mousemove",mouseMove);
-    document.addEventListener("mouseup", mouseUp)
-})
+
+    initialTop = box.offsetTop;
+    initialLeft = box.offsetLeft;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+  });
+});
 
 function mouseUp(e) {
-    document.removeEventListener("mousemove", mouseMove);
+    let isOverLaped = false;
+    const info = currentBox.getBoundingClientRect();
+    boxes.forEach((box) => {
+        const otherInfo = box.getBoundingClientRect();
+        if( (info.right > otherInfo.left && info.right < otherInfo.right && ((info.bottom > otherInfo.top && info.bottom < otherInfo.bottom) || (info.top > otherInfo.top && info.top < otherInfo.bottom))  ) || 
+            (info.left > otherInfo.left && info.left < otherInfo.right && ((info.bottom > otherInfo.top && info.bottom < otherInfo.bottom) || (info.top > otherInfo.top && info.top < otherInfo.bottom)) ) ) {
+                isOverLaped = true
+            }
+    });
+    if (isOverLaped) {
+        currentBox.style.top = initialTop + "px";
+        currentBox.style.left = initialLeft + "px";
+    }
+  document.removeEventListener("mousemove", mouseMove);
+  document.removeEventListener("mouseup", mouseUp);
+  currentBox = null;
 }
 
-function mouseMove(e){
-    newX = startX - e.clientX;
-    newY = startY - e.clientY;
+function mouseMove(e) {
+  if (currentBox === null) return;
 
-    startX = e.clientX; startY = e.clientY;
+  newX = startX - e.clientX;
+  newY = startY - e.clientY;
 
-    box.style.top = (box.offsetTop - newY) + "px";
-    box.style.left = (box.offsetLeft - newX) + "px";
+  startX = e.clientX;
+  startY = e.clientY;
+
+  currentBox.style.top = currentBox.offsetTop - newY + "px";
+  currentBox.style.left = currentBox.offsetLeft - newX + "px";
 }
-
